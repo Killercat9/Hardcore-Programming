f4f7c422c24c33287d3ef8d70cc85dadd354bf45
Philip Enhörning <shungushi@live.se>
2017-01-20 09:37:45 +0100

    Daaaayum

---
+++ Gittingthatfiles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gittingthatfiles
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Thehelloeoes theHelloeoes = new Thehelloeoes();
+            theHelloeoes.Thehelloeoesofthemost();
+        }
+    }
+}
---
+++ Gittingthatfiles/Thehelloeoes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gittingthatfiles
+{
+    class Thehelloeoes
+    {
+        string hello = "Hello World!";
+        public void Thehelloeoesofthemost()
+        {
+            for (int i = 0; i < hello.Length; i++)
+            {
+                Console.Write(hello[i]);
+            }
+        }
+    }
+
+
+}
